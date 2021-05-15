15b9cb7f15a5aeb6c26b82042e1ed0f3b9c2e254
zhangyong <zhangyong_web@163.com>
2021-05-15 10:44:58 +0800

    数据库新增编码配置

--- core/db.js
+++ core/db.js
@@ -13,13 +13,18 @@ const sequelize = new Sequelize(dbName, user, password, {
   dialect: 'mysql',
   host,
   port,
-  logging: true,
+  logging: false,
   timezone: '+08:00',
   define: {
     timestamps: true,
     createdAt: 'created_at',
     updatedAt: 'updated_at',
-    deletedAt: 'deleted_at'
+    deletedAt: 'deleted_at',
+    charset: 'utf8',
+    dialectOptions: {
+      collate: 'utf8_general_ci' 
+    },
+    timestamps: true
   }
 })
 
